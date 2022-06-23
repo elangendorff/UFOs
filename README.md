a2b37aae3c004c319f97d07d7b549a66ee00636b
Eric Langendorff <eric.langendorff@asiafoundation.org>
2022-06-23 00:15:43 -0700

    Challenge complete

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# UFOs
-Module 11: UFO Sightings with JavaScript
+# UFOs—The Truth Is Out There
+
+## Overview
+Using HTML (with Bootstrap) and JavaScript (with D3), create a Web page that dynamically filters UFO sighting data based on user-supplied criteria.
+
+## Results
+The page is relatively straightforward, containing a [fairly long list](./static/images/full_list.png) of UFO sightings.
+
+[Details on the page's use follow, with several images provided in-line, to demonstrate and clarify the page's use. In order to prevent this document from becoming too cluttered, though, other images can instead be accessed via links in the text.]
+
+### Limiting the Results
+
+#### Single-value Filters
+
+The list can be made more managable by entering values into any of the filter fields that appear on the page.
+
+By date of sighting…
+![filtered by date](./static/images/filtered_by_date.png)
+
+…by city…
+![filtered by city](./static/images/filtered_by_city.png)
+
+…or by [state](./static/images/filtered_by_state.png), [country](./static/images/filtered_by_country.png), or [shape](./static/images/filtered_by_shape.png). [Click the links at left to see images of filtered examples.]
+
+#### Multi-valued filters
+
+Users can also enter values into multiple fields, and the page will return results that satisfy each of the given criteria:
+![multiple filters example](./static/images/multiple_filters.png)
+
+## Summary
+Although the page can easily pare down the data if the user knows specifically what to look for, there are several notable limitations to it, at present.
+
+1. The filters can only check for exact matches.
+   - A partial match is insufficient. So if, for example, a user enters [west]() into the City field, it will return no matches, despite the presence of cities [west boylston](./static/images/west_boylston.png) and [westbrook](./static/images/westbrook.png) appearing in the list.
+   - A difference in capitalization counts as a different filter. [West Boylston](./static/images/West_Boylston_proper_case.png), for example, returns no results, while [west boylston](./static/images/west_boylston.png), on the other hand, returns 1 result.
+2. Multiple filters are read as `AND` only; it is not possible to filter, for example, for sightings in the state of Arkansas (ar) `OR` in the the country of Canada (ca).
+
+It shouldn't be too difficult to deal with the upper- / lowercase problem. There are many ways to convert the user's input to lowercase before sending it to the filter, behind the scenes, which would handle the issue fairly easily.
+
+The `AND`/`OR` problem can also be handled without too much trouble. It will involve adding another control to the page—some kind of "switch" that the user can toggle depending on whether `AND`- or `OR`-behavior is desired. The internal filtering mechanism will also have to change a little. Currently, with each new filtering criterion, the page just removes all those rows that don't apply. In `OR` mode, though, the code will have to work through some boolean logic regarding the several filters selected before actually applying the filter.
+
+Use of a [regular expression](https://www.regular-expressions.info/) engine could also handle the problems with partial matches, and it could also allow filtering on the Duration and Comments columns, which is currently not possible.
--- static/js/app.js
+++ static/js/app.js
@@ -1,49 +1,82 @@
-// import the data from data.js
+// from data.js
 const tableData = data;
 
-// Reference the HTML table using d3 ("Data-Driven Documents" JavaScript library)
-var tbody = d3.select("tbody"); // Selects the first element (of the HTML code that calls this script) that matches the string "tbody".
-                                // https://github.com/d3/d3-selection/blob/v3.0.0/README.md#select
+// get table references
+var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    tbody.html("");                                 // Clear any existing data inside `tbody` (other than the `<tbody/> tags, themselves).
-    data.forEach((dataRow) => {                     // For each `dataRow` in the `data` function parameter (not the `data.js` file)…
-        let row = tbody.append("tr");               // Inside the `<tbody>` element (which should exist due to the definition of the `tbody` object),
-                                                    // add (append) a table row (`<tr/>`) element, and assign it to `row`.
-        Object.values(dataRow).forEach((val) => {   //      What is `Object`???
-                                                    // For each `val` in the values of `dataRow` (i.e., for every element of `dataRow`),
-            let cell = row.append("td");            // go inside the `row` object (which has table row `<tr/>` tags, see above),
-                                                    // add (append) a table data (`<td/>`) tag,
-                                                    // assign that table data element to `cell`,
-            cell.text(val);                         // and put `val`'s text inside `cell` (i.e., inside the `<td/>` element).
-        });
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
+  });
 }
 
-function handleClick() {
-    // Select the first element with a value that matches string "datetime"
-    // (The HTML code that the function will be reading from with have tags with 'id="datetime"' in it.),
-    // get the value of that element (`.property("value")`), and assign it to `date`.
-    let date = d3.select("#datetime").property("value");
-    
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
+
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+    let theElement = d3.select(this);                                               // What is "this"?
+    // console.log(`Filtered element: ${theElement}`)
+    // 4b. Save the value that was changed as a variable.
+    let theValue = theElement.property("value");
+    console.log(`Filtered value: ${theValue}`);
+    // 4c. Save the id of the filter that was changed as a variable.
+    let theID = theElement.property("id");
+    console.log(`Filtered ID: ${theID}`);
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+    if (theValue) {   // As long as `theValue` isn't "falsy" (null, undefined, empty string, etc.)…
+      filters[theID] = theValue;
+    }
+    else {
+      delete filters[theID];
+    }
+
+    // console.log(`Filters: ${filters}`);
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
+  }
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
-    
-    // As long as `date` isn't empty…
-    if (date) {
-        // On each element (`row`) of `filteredData`,
-        // check whether its `datetime` entry matches `date`.
-        // Filter `filteredData` based on those that do,
-        // and make the filtered object the new `filteredData`.
-        filteredData = filteredData.filter(row => row.datetime === date);
-    };
-    
+  
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.keys(filters).forEach(id => {
+      filteredData = filteredData.filter(row => row[id] === filters[id] )
+    });
+  
+    // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-}
-
-// The HTML code will have a `button` element with `id="filter-btn"`.
-// (No other element should have this `id`; it should be unique.)
-// When that button is clicked, call the `handleClick` function.
-d3.selectAll("#filter-btn").on("click", handleClick);
-
-// Build the `tableData` table when the page loads
-buildTable(tableData);
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("#datetime").on("change", updateFilters);
+  d3.selectAll("#city").on("change", updateFilters);
+  d3.selectAll("#state").on("change", updateFilters);
+  d3.selectAll("#country").on("change", updateFilters);
+  d3.selectAll("#shape").on("change", updateFilters);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
